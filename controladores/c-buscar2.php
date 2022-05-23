b95e87625dcf48d60536f0adb8d3e562d24fd561
Davidsanches24 <81368662+Davidsanches24@users.noreply.github.com>
2022-05-23 14:56:02 -0500

    Add files via upload

---
+++ controladores/c-buscar2.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title></title>
+        <link rel="stylesheet" href="../css/estilo.css">
+
+        
+        
+    </head>
+
+    <body>
+    <a href="../importar.php"><button>volver</button></a>
+            <?php include( "../index1.php" ); 
+            
+
+            
+
+
+            
+            ?>
+            
+       
+
+    </body>
+</html>
